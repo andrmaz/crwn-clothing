0b75490347d4922f34441cef145f7a271b26fdc7
andrmaz <mazbsorz@gmail.com>
2020-06-01 17:12:23 +0200

    broken homepage into smaller components

---
+++ src/pages/homepage/homepage.component.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-expressions */
+import React from 'react';
+
+import Directory from '../../components/directory/directory.component';
+
+import './homepage.styles.scss';
+
+// eslint-disable-next-line no-unused-vars
+const HomePage = () => (
+    <div className='homepage'>
+      <Directory />
+    </div>
+)
+
+export default HomePage;
