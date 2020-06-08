8e0c95f393db59c8287d7998eca6a060b489d064
andrmaz <mazbsorz@gmail.com>
2020-06-08 21:30:15 +0200

    updating application with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toogleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss'
 
-const CartIcon = ({ toogleCartHidden }) => (
+const CartIcon = ({ toogleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={ toogleCartHidden }>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{ itemCount }</span>
     </div>
 )
 
@@ -18,7 +19,11 @@ const mapDispatchToProps = dispatch => ({
     toogleCartHidden: () => dispatch(toogleCartHidden())
 });
 
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
+})
+
 export default connect(
-    null, 
+    mapStateToProps, 
     mapDispatchToProps
 )(CartIcon);
--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 import  CartIcon  from '../cart-icon/cart-icon.component';
-import CartDropdown from '../cart-dropdown/cart-dropdown';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
