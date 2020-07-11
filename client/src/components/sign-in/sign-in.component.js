093b78aa081f7cbfdf264a0c913ccfc95b95ffe5
andrmaz <mazbsorz@gmail.com>
2020-07-11 19:03:40 +0200

    fixed signInWithGoogle button mobile view

--- client/src/components/sign-in/sign-in.component.js
+++ client/src/components/sign-in/sign-in.component.js
@@ -63,7 +63,7 @@ class SignIn extends Component{
                     />
                     <div className='buttons'>
                         <CustomButton type='submit'> Sign In </CustomButton>
-                        <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn> 
+                        <CustomButton className='googleButton' type="button" onClick={signInWithGoogle} isGoogleSignIn> 
                             Sign in with Google
                         </CustomButton>
                     </div>
